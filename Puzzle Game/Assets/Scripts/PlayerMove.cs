3f4ddc5ee2e2e33e1941f819eb79ba80853dd754
Hazar <92859817+HazarBakir@users.noreply.github.com>
2023-07-14 15:38:56 +0300

    new astronaut anims updated.
    Jumping and walking anims added

--- Puzzle Game/Assets/Scripts/PlayerMove.cs
+++ Puzzle Game/Assets/Scripts/PlayerMove.cs
@@ -7,41 +7,36 @@ public class PlayerMove : MonoBehaviour
 {
     private float horizontal;
     private float vertical = 16f;
-    private float speed = 5f;
+    private float speed = 8f;
     private bool FacingRight = true;
-
-
     Animator animator;
     [SerializeField] Rigidbody2D rb2d;
     [SerializeField] Transform groundCheck;
     [SerializeField] LayerMask groundlayer;
-
-    // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
     }
-
-    // Update is called once per frame
     void Update()
     {
         horizontal = Input.GetAxisRaw("Horizontal");
 
-        if (Input.GetButtonDown("Jump") && isGrounded())
+        if (Input.GetKeyDown(KeyCode.W) && IsGrounded())
         {
             rb2d.velocity = new Vector2(rb2d.velocity.x, vertical);
         }
-        if (Input.GetButtonUp("Jump") && rb2d.velocity.y > 0f)
+        if (Input.GetKeyUp(KeyCode.W) && rb2d.velocity.y > 0f)
         {
             rb2d.velocity = new Vector2(rb2d.velocity.x, rb2d.velocity.y * 0.5f);
+            animator.SetBool("isJumped", true);
         }
         Flip();
+        PlayerMoveAnimation();
     }
     private void FixedUpdate()
     {
         rb2d.velocity = new Vector2(horizontal * speed, rb2d.velocity.y);
     }
-
     private void Flip()
     {
         if (FacingRight && horizontal < 0f || !FacingRight && horizontal > 0f)
@@ -53,8 +48,28 @@ public class PlayerMove : MonoBehaviour
         }
     }
 
-    private bool isGrounded()
+    private bool IsGrounded()
     {
         return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundlayer);
+    }
+    public void PlayerMoveAnimation()
+    {
+        if (horizontal != 0)
+        {
+            animator.SetBool("isWalking", true);
+        }
+        else
+        {
+            animator.SetBool("isWalking", false);
+        }
+        if (IsGrounded())
+        {
+            animator.SetBool("isJumped", false);
+        }
+        else
+        {
+            animator.CrossFade("Astronaut_Jump", 0.1f);
+            animator.SetBool("isJumped", true);
+        }
     }
 }
