204e264791f9ebcc08fbffdfe5becf5198820fcb
Hazar <92859817+HazarBakir@users.noreply.github.com>
2023-10-21 13:38:44 +0300

    Create GameStarAnim
    merge the GameStartAnim and IntroductionText

--- Puzzle Game/Assets/Scripts/CanvasButtons.cs
+++ Puzzle Game/Assets/Scripts/CanvasButtons.cs
@@ -5,10 +5,15 @@ using UnityEngine;
 public class CanvasButtons : MonoBehaviour
 {
     public GameObject PanelGameObject;
+    public GameObject GameStoryText;
+    public Animator MessageAnimationAnimator;
 
     public void ActivatePanel()
     {
         PanelGameObject.SetActive(true);
+        GameStoryText.SetActive(true);
+        MessageAnimationAnimator.SetBool("ClickedPlayButton", true);
+        Debug.Log("hi");
     }
 
 }
