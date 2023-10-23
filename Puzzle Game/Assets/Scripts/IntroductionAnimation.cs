cd821596df7aa545450b9faf165d81ef431c8d11
Hazar <92859817+HazarBakir@users.noreply.github.com>
2023-10-23 20:05:46 +0300

    Edit and fix the IntroductionScene bugs

--- Puzzle Game/Assets/Scripts/IntroductionAnimation.cs
+++ Puzzle Game/Assets/Scripts/IntroductionAnimation.cs
@@ -2,16 +2,25 @@
 using System.Collections.Generic;
 using System.Numerics;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using Vector3 = UnityEngine.Vector3;
 
 public class IntroductionAnimation : MonoBehaviour
 {
 
     public float Speed = 1.0f;
+    public float CountdownDuration = 15.0f;
 
     void Update()
     {
         transform.Translate(Vector3.up * Speed * Time.deltaTime);
 
+        CountdownDuration -= Time.deltaTime;
+
+        if (CountdownDuration <= 0)
+        {
+            SceneManager.LoadScene("FirstLevel");
+
+        }
     }
 }
